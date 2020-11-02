db1239dd95435d9728b7b887ac61eda3f155f82d
Saurabh <saurabh.nit20@gmail.com>
2020-11-02 14:32:02 +0530

    [Saurabh].Employees PayRoll Services

--- PayRollService.sql
+++ PayRollService.sql
@@ -1,52 +1,51 @@
 --UC1_Creation_of_Payroll_Services_Database
-create database payroll_services;
-
+create database PayRollService;
 
 --UC2_Create_Employee_Payroll_Table
-use payroll_service;
+use PayRollService;
 
-create table employee_payroll
+create table Employee_PayRoll
 (
 id int identity(1,1),
 name varchar(25) not null,
-salary money not null,
+salary money,
 start date not null
 );
 
-
 --UC3_InsertintoEmployeePayrollTable
-insert into employee_payroll values
+insert into Employee_PayRoll values
 ('Bill',100000.00,'2018-01-03'),
 ('Terisa',200000.00,'2019-11-03'),
 ('Charlie',300000.00,'2020-05-21');
 
-
 --UC4_Retrieve_Data_from_EmplyeeTable
-select * from employee_payroll;
+select * from Employee_PayRoll;
 
 
 --UC5_Retrieve_Data_Using_Condition
-select * from employee_payroll
+select * from Employee_PayRoll
 where name = 'Bill';
 
-select * from employee_payroll
-where start between '2020-01-01' and GETDATE();
+select * from Employee_PayRoll
+where start between CAST('2020-01-01' AS date) and GETDATE();
+
 
 
 --UC6_Update_Existing_Rows_and_Add new_column
-ALTER TABLE employee_payroll
+ALTER TABLE Employee_PayRoll
 ADD gender char;
 
-select * from employee_payroll;
+select * from Employee_PayRoll;
 
-UPDATE employee_payroll
+UPDATE Employee_PayRoll
 SET gender = 'M'
-where id in (1,2);
+where name = 'Bill';
 
-UPDATE employee_payroll
+UPDATE Employee_PayRoll
 SET gender = 'F'
-where id in (3,4);
+where name = 'Charlie';
 
+select * from Employee_PayRoll;
 
 --UC7_Use_DB_Functions
 SELECT gender, SUM(salary) FROM employee_payroll
